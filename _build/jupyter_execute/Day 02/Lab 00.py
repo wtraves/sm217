7be90f919874cdcb8c5de007764b61e7a4820861
William Traves <traves@itsdadminsimac.myfiosgateway.com>
2021-11-16 21:57:16 -0500

    adding note for lab00

---
+++ _build/jupyter_execute/Day 02/Lab 00.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Lab 00 and Reading for Next Class
+# 
+# In today's class we'll work on our first lab, Lab 00. You can find the Jupyter notebook containing the lab on our [course website](https://sites.google.com/usna.edu/data-science-for-decision-make/home). The lab introduces notebooks, basic arithmetic, syntax errors, use of functions (particularly the percentile function), and concludes with an examination of data about income inequality in America. 
+# 
+# Please read [Chapter 2](https://inferentialthinking.com/chapters/02/causality-and-experiments.html) of the Computational and Inferential Thinking textbook prior to next class. The chapter compares observational studies and randomized clinical trials. The kind of study determines what kinds of conclusions you can reach. Complete Reading Quiz 02 before next class. 
